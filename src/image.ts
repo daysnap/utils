82712b07548b672502d072b2ecd4eadc860c30e4
chenzhigang <979703986@qq.com>
2022-04-18 23:27:19 +0800

    feat: image tofile

--- src/image.ts
+++ src/image.ts
@@ -6,7 +6,7 @@ export const toBase64: (blob: Blob) => Promise<FileReader> = blob => new Promise
   fileReader.onerror = reject
 })
 
-export const toFile = (base64: string, type: 'png | gif') => {
+export const toFile = (base64: string, contentType?: string): Blob => {
   const binaryString = window.atob(base64.split(',')[1])
   const arrayBuffer = new ArrayBuffer(binaryString.length)
   const uint8Array = new Uint8Array(arrayBuffer)
@@ -16,12 +16,26 @@ export const toFile = (base64: string, type: 'png | gif') => {
   let blob
   try {
     blob = new Blob([uint8Array])
-  } catch (e) {
-    const BlobBuilder =
-    window.BlobBuilder
+  } catch (e: any) {
+    if (e.name === 'TypeError') {
+      const win = (window as any)
+      const BlobBuilder: any = win.BlobBuilder
+        || win.WebKitBlobBuilder
+        || win.MozBlobBuilder
+        || win.MSBlobBuilder
+      if (BlobBuilder) {
+        const blobBuilder = new BlobBuilder()
+        blobBuilder.append(arrayBuffer)
+        blob = blobBuilder.getBlob(contentType)
+      }
+    } else {
+      throw new Error('版本过低')
+    }
   }
+  return blob
 }
 
 export default {
   toBase64,
+  toFile,
 }
