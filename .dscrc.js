a78b8afa36b1b114dfc6741f57639fbf42bed439
chenzhigang <979703986@qq.com>
2022-04-21 16:22:55 +0800

    feat: dsc build esm cjs

---
+++ .dscrc.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+  cjs: {
+    src: [
+      'src/**/*.ts'
+    ],
+    dest: 'lib',
+  },
+  esm: {
+    src: [
+      'src/**/*.ts'
+    ],
+    dest: 'es'
+  }
+}
--- es/pick.d.ts
+++
@@ -1,1 +0,0 @@
-export default function pick<T extends object, K extends keyof T>(obj: T, fields?: K[]): Pick<T, K>;
