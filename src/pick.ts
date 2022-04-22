48f18a3c11edb5a69ad096be92a2df3d6636b007
chenzhigang <979703986@qq.com>
2022-04-22 16:17:05 +0800

    refactor: 去掉 debug

--- src/pick.ts
+++ src/pick.ts
@@ -3,7 +3,7 @@ export default function pick<T extends object, K extends keyof T> (
   obj: T,
   fields: K[] = []
 ): Pick<T, K> {
-  const clone = {...obj} as Pick<T, K>
+  const clone = {} as Pick<T, K>
 
   fields.forEach(key => clone[key] = obj[key])
 
