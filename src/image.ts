a7a927e1a977c21721f075e7972f8937f83378a2
chenzhigang <979703986@qq.com>
2022-04-22 22:57:37 +0800

    feat: image 50%

--- src/image.ts
+++ src/image.ts
@@ -1,12 +1,14 @@
 
-export const toBase64: (blob: Blob) => Promise<FileReader> = blob => new Promise((resolve, reject) => {
+import pick from './pick'
+
+export const toBase64 = (file: File) => new Promise<FileReader>((resolve, reject) => {
   const fileReader = new FileReader()
-  fileReader.readAsDataURL(blob)
+  fileReader.readAsDataURL(file)
   fileReader.onload = (res) => resolve(fileReader)
   fileReader.onerror = reject
 })
 
-export const toFile = (base64: string, contentType?: string): Blob => {
+export const toFile = (base64: string, contentType?: string): File => {
   const binaryString = window.atob(base64.split(',')[1])
   const arrayBuffer = new ArrayBuffer(binaryString.length)
   const uint8Array = new Uint8Array(arrayBuffer)
@@ -35,7 +37,42 @@ export const toFile = (base64: string, contentType?: string): Blob => {
   return blob
 }
 
+interface CompressOptions {
+  width?: number
+  height?: number
+  quality?: number
+}
+
+interface CompressSuccessResult {
+  base64: string
+  width: string
+  height: string
+  image: HTMLImageElement
+}
+
+export const compress = (image: CanvasImageSource, options: CompressOptions = {}): string => {
+  const props = pick(image, ['width', 'height'])
+  const { width, height, quality } = Object.assign({}, props, options)
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+  canvas.width = width
+  canvas.height = height
+  ctx.fillStyle = `#fff`
+  ctx.fillRect(0, 0, width, height)
+  ctx.drawImage(image, 0, 0, width, height)
+  return canvas.toDataURL('image/jpeg', quality)
+}
+
+export const getImageElement = (src: string) => new Promise<HTMLImageElement>((resolve, reject) => {
+  const image = new Image()
+  image.onload = () => resolve(image)
+  image.onerror = reject
+  image.src = src
+})
+
 export default {
   toBase64,
   toFile,
+  compress,
+  getImageElement,
 }
