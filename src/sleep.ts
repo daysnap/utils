a0d6f826e075373dff46201152c256694bd1c621
chenzhigang <979703986@qq.com>
2022-12-04 20:32:34 +0800

    chore: 简写

--- src/sleep.ts
+++ src/sleep.ts
@@ -3,6 +3,6 @@
  */
 export function sleep(timeout: number, isSuccess = true) {
   return new Promise<void>((resolve, reject) => {
-    setTimeout(() => (isSuccess ? resolve() : reject()), timeout)
+    setTimeout(isSuccess ? resolve : reject, timeout)
   })
 }
