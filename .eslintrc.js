58f19a5babc42aa3948a14bcd22fe40c01d6df84
chenzhigang <979703986@qq.com>
2022-12-04 12:14:08 +0800

    chore: 修改eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
   },
   settings: {
     'import/resolver': {
