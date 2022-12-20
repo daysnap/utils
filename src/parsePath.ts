55f426ce02655c4d6b2b502753b4f0bed32f4877
chenzhigang <979703986@qq.com>
2022-12-20 14:00:00 +0800

    fix: parse path reslut type

--- src/parsePath.ts
+++ src/parsePath.ts
@@ -1,10 +1,10 @@
 /**
  * 根据路径解析值
  */
-export function parsePath(obj: Record<string, any>, path: string) {
+export function parsePath<T = any>(obj: Record<string, any>, path: string) {
   path.split('.').forEach((key) => {
     // eslint-disable-next-line no-param-reassign
     obj = obj[key] ?? ''
   })
-  return obj
+  return obj as T
 }
