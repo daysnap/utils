64a6d638a2598f779ffffff2a7ea3a5010ea9cb9
智兵 <bing.zhi@autostreets.com>
2022-12-27 17:06:55 +0800

    feat: 字符串去空格、isWeixin、资源加载

---
+++ src/stringTrim.ts
@@ -0,0 +1,20 @@
+/**
+ *去除字符串空格
+ */
+export function stringTrim(str: string, type?: 1 | 2 | 3 | 4) {
+  // 1-所有空格，2-前后空格，3-前空格，4-后空格
+  // eslint-disable-next-line no-param-reassign
+  type = type ?? 1
+  switch (type) {
+    case 1:
+      return str.replace(/\s+/g, '')
+    case 2:
+      return str.replace(/(^\s*)|(\s*$)/g, '')
+    case 3:
+      return str.replace(/(^\s*)/g, '')
+    case 4:
+      return str.replace(/(\s*$)/g, '')
+    default:
+      return str
+  }
+}
