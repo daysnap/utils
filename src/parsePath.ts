dd47b832b44feba0b55bef0c54158bee87f48bb3
chenzhigang <979703986@qq.com>
2023-03-20 20:51:33 +0800

    v0.0.44 fix:修复parsePath bug

--- src/parsePath.ts
+++ src/parsePath.ts
@@ -1,13 +1,17 @@
+import { isObject } from './isObject'
+
 /**
  * 根据路径解析值
  */
 export function parsePath<T = any>(obj: Record<string, any>, path: string): T {
   const segments = path ? path.split('.') : []
 
-  segments.forEach((key) => {
-    // eslint-disable-next-line no-param-reassign
-    obj = obj[key]
-  })
+  for (let i = 0, len = segments.length; i < len; i++) {
+    obj = obj[segments[i]]
+    if (!isObject(obj)) {
+      break
+    }
+  }
 
   return obj as T
 }
