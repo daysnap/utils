bb65e6238bca67ad36ac77bf972efbe4f7244af3
chenzhigang <979703986@qq.com>
2023-04-03 09:08:37 +0800

    v0.0.45 fix: throttle params type

--- src/throttle.ts
+++ src/throttle.ts
@@ -2,7 +2,7 @@
  * 节流函数
  * 减少事件执行次数，有规律的执行
  */
-export function throttle<T extends (...args: []) => any>(fn: T, ms: number) {
+export function throttle<T extends (...args: any[]) => any>(fn: T, ms: number) {
   let timer: any = null
 
   // eslint-disable-next-line func-names
