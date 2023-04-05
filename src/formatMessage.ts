edb8efd101ec0ef168f0a78cbe9a90f00a8d9b5d
chenzhigang <979703986@qq.com>
2023-04-05 10:05:31 +0800

    v0.0.47 feat format message

---
+++ src/formatMessage.ts
@@ -0,0 +1,32 @@
+import { isNull } from './isNull'
+import { isObject } from './isObject'
+import { isUndefined } from './isUndefined'
+import { parsePath } from './parsePath'
+
+/**
+ * 格式化消息
+ * formatMessage({ a: '123', b: { ba: '456' } }, ['a']) => '123'
+ * formatMessage({ a: '123', b: { ba: '456' } }, ['b.ba']) => '456'
+ */
+export function formatMessage(
+  message: unknown,
+  keys: string[] = ['message', 'msg', 'errorMsg'],
+): string {
+  if (isNull(message)) {
+    return ''
+  }
+
+  if (!isObject(message)) {
+    return `${message}`
+  }
+
+  for (let i = 0, len = keys.length; i < len; i++) {
+    const key = keys[i]
+    const val = parsePath(message, key)
+    if (!isUndefined(val)) {
+      return `${val}`
+    }
+  }
+
+  return JSON.stringify(message)
+}
