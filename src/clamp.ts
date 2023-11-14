8a506636d2963209269ea9dd392a45d456574750
chenzhigang <979703986@qq.com>
2023-11-14 11:04:37 +0800

    v0.0.67 clamp

---
+++ src/clamp.ts
@@ -0,0 +1,15 @@
+/**
+ * 首选值超出最小值，返回最小值；首选值超出最大值，返回最大值；其余返回首选值
+ * @param min 最小值
+ * @param x 首选值
+ * @param max 最大值
+ */
+export function clamp(min: number, val: number, max: number) {
+  if (val < min) {
+    return min
+  }
+  if (val > max) {
+    return max
+  }
+  return val
+}
