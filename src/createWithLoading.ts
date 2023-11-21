b09822c1120a336a84f20bd19e233a0556728ae3
chenzhigang <979703986@qq.com>
2023-11-21 10:28:31 +0800

    v0.0.68 createwithloading

---
+++ src/createWithLoading.ts
@@ -0,0 +1,33 @@
+import { Loose } from '@daysnap/types'
+
+/**
+ * loading 生成器
+ * const withLoading = createWithLoading(() => showLoading())
+ * const fn = withLoading(async () => { // ... })
+ * fn() 执行的时候就会执行showLoading
+ */
+export function createWithLoading<O = any>(
+  showLoading: (options: O) => Loose<{ close: () => any }>,
+) {
+  return function withLoading<T extends (...args: any[]) => Promise<any>>(
+    fn: T,
+    options?: O,
+  ) {
+    return async (
+      ...params: [...Parameters<T>, O?]
+    ): Promise<Awaited<ReturnType<T>>> => {
+      if (params.length > fn.length) {
+        options = params.pop() as O
+      }
+      const toast = options ? showLoading(options) : null
+      try {
+        return await fn(params)
+        // eslint-disable-next-line no-useless-catch
+      } catch (error) {
+        throw error
+      } finally {
+        toast?.close()
+      }
+    }
+  }
+}
