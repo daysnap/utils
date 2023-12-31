e35a591686fcfb7336923b845245884b6123e5f5
chenzhigang <979703986@qq.com>
2023-12-31 14:17:28 +0800

    feat: 节流函数更新

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,8 +1,31 @@
 /**
- * 节流函数
- * 减少事件执行次数，有规律的执行
+ * 节流函数，时间戳模式
+ * 减少事件执行次数，有规律的执行，事件触发后立即执行
  */
-export function throttle<T extends (...args: any[]) => any>(fn: T, ms: number) {
+export function throttleLeading<T extends (...args: any[]) => any>(
+  fn: T,
+  ms: number,
+) {
+  let pre = Date.now()
+
+  // eslint-disable-next-line func-names
+  return function (this: unknown, ...args: Parameters<T>) {
+    const now = Date.now()
+    if (now - pre >= ms) {
+      fn.apply(this, args)
+      pre = now
+    }
+  }
+}
+
+/**
+ * 节流函数，定时器模式
+ * 减少事件执行次数，有规律的执行，事件触发后延迟执行
+ */
+export function throttleTrailing<T extends (...args: any[]) => any>(
+  fn: T,
+  ms: number,
+) {
   let timer: any = null
 
   // eslint-disable-next-line func-names
@@ -16,4 +39,16 @@ export function throttle<T extends (...args: any[]) => any>(fn: T, ms: number) {
       timer = null
     }, ms)
   }
+}
+
+/**
+ * 节流函数
+ * 减少事件执行次数，有规律的执行
+ */
+export function throttle<T extends (...args: any[]) => any>(
+  fn: T,
+  ms: number,
+  mode: 'leading' | 'trailing' = 'trailing',
+) {
+  return mode === 'leading' ? throttleLeading(fn, ms) : throttleTrailing(fn, ms)
 }
