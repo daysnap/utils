2e2a643f6050bf63e02a0f5bdb6e0b698621c3ea
chenzhigang <979703986@qq.com>
2025-10-29 15:55:35 +0800

    chore: 扩展 sleep 返回数据参数

--- src/sleep.ts
+++ src/sleep.ts
@@ -2,9 +2,10 @@
  * 休眠方法
  * @param ms 休眠时长 毫秒
  * @param isSuccess 默认执行成功
+ * @param res 数据
  */
-export function sleep(ms: number, isSuccess = true) {
+export function sleep(ms: number, isSuccess = true, res?: any) {
   return new Promise<void>((resolve, reject) => {
-    setTimeout(isSuccess ? resolve : reject, ms)
+    setTimeout(isSuccess ? resolve : reject, ms, res)
   })
 }
